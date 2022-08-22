5ba8c5c6f39f6a88c05dc57ba2b52e47511ff626
DARK_ALPHA_XTEAM <106463443+darkalphaxteam@users.noreply.github.com>
2022-08-22 14:53:59 +0530

    📩 TESTING 📩

---
+++ settings.js
@@ -0,0 +1,135 @@
+//CYBER-X  DARKALPHAXTEAM ◎◎◎ https://github.com/darkalphaxteam\\
+//══════════════THANKS FOR CHEEMS BOT MD (DGNEON)═ALL THE CREADITS GOES TO HIM═════════════\\
+// EXAM ඉවර උනාම ඉතුරු ටික දෙන්නම් 😓😓😓 GOOD LUCK ALL\\
+const fs = require('fs')
+const chalk = require('chalk')
+
+//API WEBSITE \\
+global.APIs = {
+	zenz: 'https://zenzapis.xyz',
+}
+
+//API KEY\\
+global.APIKeys = {
+	'https://zenzapis.xyz': 'your key',
+}
+
+
+//AUTO FUNCTIONS 
+
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = false //auto reading in gc (true to on, false to off)
+global.autobio = false  // auto bio system checking on Developing Leval
+global.autoRecord = false //auto recording (true to on, false to off)
+global.available = true //auto available (true to on, false to off)
+
+//VIDEO MENU 
+global.vidmenu = fs.readFileSync("./LocalData/theme/cyber.mp4") //gif and video menu
+
+//DOCUMENT VARIANTS
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+//OWNER CONTACT
+global.vcardowner = ['94711421243'] //ur owner number
+global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
+global.ytname = "YT: TGM Arcade" //ur yt chanel name
+global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
+global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+
+//BOT COSTOMIZEDTATION
+global.owner = ['94711421243']
+global.ownertag = '9471421243' //ur tag number
+global.botname = 'CYBER-X TEST EDITION' //ur bot name
+global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
+global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
+global.sc = 'https://github.com/darkalphaxteam' //script link
+global.linkz = 'https://youtu.be/uzMbW5v9QLE'
+global.reactmoji = "😘" //ur menu react emoji
+global.themeemoji = "🚀" //ur theme emoji
+global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
+global.author = "DARK ALPHA" //ur sticker watermark author
+global.aliveimg ="https://telegra.ph/file/71c859102f3625d06204b.jpg"
+global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
+global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
+global.gbyeimg = "https://telegra.ph/file/844812c77704231c08a90.jpg"
+
+//BOT MEDIAS
+global.thum = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
+global.log0 = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur error pic
+global.thumb = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
+
+//DATA BASED
+
+global.premium = ['94711421243'] //ADD YOU NUMBER
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+//omther
+global.sessionName = 'session'
+global.antitags = true
+global.prefa = ['','!','.']
+global.sp = '⭔'
+global.mess = {
+    success: '✉️✏️ _Done_',
+    admin: '✉️✏️ _This Feature Is Only For Admin!_',
+    botAdmin: '✉️✏️ _Bot Must Be Admin First!_',
+    owner: '✉️✏️ _This Feature Is Only For Owner!_',
+    group: '✉️✏️ _Feature Used Only For Groups!_',
+    private: '✉️✏️ _Features Used Only For Private Chat!_',
+    bot: '✉️✏️ _This Feature Is Only For Bot_',
+    wait: '✉️✏️ _Processing Please Wait..._',
+    linkm: '📧✏️ _Where is the link?_',
+    error: '✉️🤕 ```Error!!```',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+    ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
+    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
+    banChat: 'The bot was banned in this group, please contact the owner to unban'
+}
+    global.limitawal = {
+    premium: "Infinity",
+    free: 12,
+    monayawal: 1000
+}
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+   umpanawal: 5,
+   potionawal: 1
+}
+global.thumb = fs.readFileSync('./LocalData/theme/cyber.jpg')
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
