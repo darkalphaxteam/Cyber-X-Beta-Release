a02edde55fed026babca7f99d627c1dec309a956
alpha1xenox <112790399+alpha1xenox@users.noreply.github.com>
2022-09-08 20:32:35 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,31 +36,31 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
-global.ytname = "YT: TGM Arcade" //ur yt chanel name
-global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
-global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
+global.vcardowner = ['919776517392'] //ur owner number
+global.ownername = "✨SOUMYA⚡" //ur owner name
+global.ytname = "YT: SG GAMING" //ur yt chanel name
+global.socialm = "GitHub: alpha1xenox" //ur github or insta name
+global.location = "INDIA" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
-global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
-global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
-global.sc = 'https://github.com/darkalphaxteam' //script link
-global.linkz = 'https://youtu.be/uzMbW5v9QLE'
+global.owner = ['919776517392']
+global.ownertag = '919776517392' //ur tag number
+global.botnumber = '12565706983' //Add your Bot Number For Log
+global.botname = 'ALPHA-X' //ur bot name
+global.ig = "https://www.instagram.com/Itz_soumya_s14/" //your theme url which will be displayed on whatsapp
+global.myweb = "https://www.instagram.com/Itz_soumya_s14/" //ur website to be displayed
+global.sc = 'https://github.com/alpha1xenox' //script link
+global.linkz = 'https://www.instagram.com/Itz_soumya_s14/'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
-global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
-global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
-global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.gbyeimg = "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.alivemsg = "ඔබ සතුව නැති දේ ලබා ගැනීමේ පහසුම ක්‍රමය  ඇගයීමයි.. හැකි සෑම විටම තව කෙනෙක්ව ඇගයීමට උත්සහ කරන්න. එය ඔබේ මානසික සංවර්ධිත බවේ සලකුණකි..\nසංස්කරණය කළ පරිවර්තනයකි...\n@best_m_otivation\n#lifetips\n#psychology\n#questions"
+global.packname = "ALPHA-X" //ur sticker watermark packname
+global.author = "LORD ALPHA" //ur sticker watermark author
+global.footer = "® ALPHA-X DEVELOPED BY SOUMYA"
+global.aliveimg ="https://telegra.ph/file/98c6e7ee73ee3827dcb4e.jpg"
+global.allmenuimg ="https://telegra.ph/file/708259469e562c809d991.jpg"
+global.welcomeimg= "https://telegra.ph/file/7393a4cadf98ca27e434e.jpg"
+global.gbyeimg = "https://telegra.ph/file/a3647718eb371f794a715.jpg"
+global.alivemsg = "HI FRIENDS IAM ONLINE ❤️"
 
 //BOT MEDIAS
 global.thum = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
@@ -70,7 +70,7 @@ global.thumb = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
 
 //DATA BASED
 
-global.premium = ['94711421243'] //ADD YOU NUMBER
+global.premium = ['919776517392'] //ADD YOU NUMBER
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
