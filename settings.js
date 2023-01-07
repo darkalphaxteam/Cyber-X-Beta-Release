c1dd9af9b7dcc5ce317a6d98e02b584358d445e1
Fw437f <118462249+Fw437f@users.noreply.github.com>
2023-01-07 23:42:29 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,15 +36,15 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['4915212908434'] //ur owner number
+global.vcardowner = ['4915212908434'] -- Add multiple numbers like  ['4915735426173']
 global.ownername = "MAGIC" //ur owner name
 global.ytname = "YT: NOT FOUND" //ur yt chanel name
 global.socialm = "GitHub: Fw437f" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['4915212908434']
-global.ownertag = '4915212908434' //ur tag number
+global.owner = ['4915212908434'] -- Add multiple numbers like  ['4915735426173']
+global.ownertag = '4915212908434' -- Add multiple numbers like  ['4915735426173']
 global.botnumber = '14342694372' //Add your Bot Number For Log
 global.botname = 'CYBER-X' //ur bot name
 global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
@@ -53,14 +53,14 @@ global.sc = 'https://github.com/darkalphaxteam' //script link
 global.linkz = 'https://youtu.be/uzMbW5v9QLE'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
-global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "MAGIC" //ur sticker watermark author
+global.packname = "✨️MAGIC✨️" //ur sticker watermark packname
+global.author = "Erstellt Für" //ur sticker watermark author
 global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
 global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
 global.gbyeimg = "https://telegra.ph/file/844812c77704231c08a90.jpg"
-global.alivemsg = "ඔබ සතුව නැති දේ ලබා ගැනීමේ පහසුම ක්‍රමය  ඇගයීමයි.. හැකි සෑම විටම තව කෙනෙක්ව ඇගයීමට උත්සහ කරන්න. එය ඔබේ මානසික සංවර්ධිත බවේ සලකුණකි..\nසංස්කරණය කළ පරිවර්තනයකි...\n@best_m_otivation\n#lifetips\n#psychology\n#questions"
+global.alivemsg = "*Bot ist Online*...\n@best_m_otivation\n#lifetips\n#psychology\n#questions"
 
 //BOT MEDIAS
 global.thum = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
